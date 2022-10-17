290edc5043691991d022e0a7e2dab5d16ef1b502
Ljcs1999 <Ljcs_1999@hotmail.com>
2022-10-16 22:19:50 -0700

    Ejercicio

---
+++ Ejercicio2.js
@@ -0,0 +1,10 @@
+let numero = parseInt(prompt("Ingrese un numero para saber si le podemos fiar:"));
+
+if(numero>12){
+    var i = "Hoy no se fia, mañana si.";
+}
+else{
+    i = "Con gusto le damos fiado.";
+}
+
+alert(i);
